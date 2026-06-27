d03ad842903193bc92e0228d657f86eabbf446c2
Waldemar Neto <waldemarnt@gmail.com>
2026-06-27 10:32:00 -0300

    chore(client): scaffold @nx/vite three + sdk project

---
+++ client/vite.config.ts
@@ -0,0 +1,36 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../node_modules/.vite/client',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [nxViteTsPaths()],
+  build: {
+    outDir: '../dist/client',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    passWithNoTests: true,
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../coverage/client',
+      provider: 'v8',
+    },
+  },
+});
