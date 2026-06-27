b7a8c24f827b5fd2666c1bde4a11e86447b163e1
Waldemar Neto <waldemarnt@gmail.com>
2026-06-27 19:31:42 -0300

    feat(game-core): add effectivePAtk and equipped damage anchor

--- libs/game-core/src/index.ts
+++ libs/game-core/src/index.ts
@@ -3,6 +3,7 @@ export * from './world-constants';
 export * from './validate-move-intent';
 export * from './seeded-rng';
 export * from './combat/starter-combat';
+export * from './combat/effective-patk';
 export * from './combat/melee-damage';
 export * from './combat/attack-timing';
 export * from './combat/combat-range';
